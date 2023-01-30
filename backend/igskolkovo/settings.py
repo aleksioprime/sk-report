1ae70633a449bf32e745fdcbc764ba563d1d46b4
Aleksei S <aleksioprime@gmail.com>
2023-01-31 01:05:18 +0300

    edit ip

--- backend/igskolkovo/settings.py
+++ backend/igskolkovo/settings.py
@@ -33,8 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080", "http://localhost:8000", 
-    "http://176.57.218.9:8080", "http://176.57.218.9:8000", 
-    "0.0.0.0:8080", "0.0.0.0:8000",
+    "http://176.57.218.9:8080", "http://176.57.218.9:8000",
 ]
 # Application definition
 
