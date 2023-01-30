b4b14883f9ffaad9a19e80e9256fe5e84b147971
Aleksei S <aleksioprime@gmail.com>
2023-01-31 01:58:34 +0300

    localhost

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://0.0.0.0:8000',
+    proxy: 'http://localhost:8000',
   }
 })
