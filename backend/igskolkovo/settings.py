5f2d6c32c56f61b27c944102ba0ad70ab2c205ed
Aleksei S <aleksioprime@gmail.com>
2023-01-31 01:17:53 +0300

    csrf edit

--- backend/igskolkovo/settings.py
+++ backend/igskolkovo/settings.py
@@ -35,6 +35,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080", "http://localhost:8000", 
     "http://176.57.218.9:8080", "http://176.57.218.9:8000",
 ]
+CSRF_TRUSTED_ORIGINS=['http://176.57.218.9']
+
+
 # Application definition
 
 INSTALLED_APPS = [
